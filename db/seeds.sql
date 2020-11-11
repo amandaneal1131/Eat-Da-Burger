902f5eb1a69e96e3943cc8b5daca8e70f9a8b81c
amandaneal1131 <amandaneal1131@gmail.com>
2020-11-11 14:36:21 -0600

    add files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', FALSE);
